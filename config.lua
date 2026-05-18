9063e7391b5f01aa2ef0ceb4a2e536a34d07191e
0627169609aa <0627169609.aa@gmail.com>
2026-05-18 04:39:54 +0100

    ➕ implement logger setup with rotation

--- config.lua
+++ config.lua
@@ -1,32 +1,48 @@
--- Configuration Loader with Defaults
+-- Logger configuration for web3-dev-tools
 
-local Config = {}
+local lfs = require('lfs')
 
--- Default Configuration
-local defaultConfig = {
-    apiUrl = "https://default.api.url",
-    maxPlayers = 100,
-    gameMode = "casual",
-    debug = false
-}
+local Logger = {}  
+Logger.__index = Logger
 
--- Load Configuration from file or use defaults
-function Config.loadConfig(filePath)
-    local file = io.open(filePath, "r")
-    if not file then
-        return defaultConfig  -- Return defaults if file not found
-    end
-    local content = file:read("*a")
-    file:close()
-
-    local config, err = load(content)
-    if err then
-        print("Error loading config: ", err)
-        return defaultConfig  -- Return defaults if there's an error
-    end
-
-    setmetatable(config, {__index = defaultConfig})
-    return config
+-- Initialize the logger with filename and rotation settings
+function Logger:new(filename, maxSize)
+    local obj = {}
+    setmetatable(obj, self)
+    self.filename = filename or 'app.log'
+    self.maxSize = maxSize or 1048576  -- Default to 1 MB
+    self.currentFileSize = 0
+    return obj
 end
 
-return Config
+-- Check the file size and rotate if necessary
+function Logger:checkRotation()
+    local file = io.open(self.filename, 'r')
+    if file then
+        file:seek('end')
+        self.currentFileSize = file:tell()
+        file:close()
+    end
+    if self.currentFileSize >= self.maxSize then
+        self:rotate()
+    end
+end
+
+-- Rotate the log file
+function Logger:rotate()
+    local oldFileName = self.filename .. '.' .. os.date('%Y%m%d%H%M%S')
+    os.rename(self.filename, oldFileName)
+    self.currentFileSize = 0
+end
+
+-- Log a message to the file
+function Logger:log(message)
+    self:checkRotation()
+    local file = io.open(self.filename, 'a')
+    if file then
+        file:write(os.date('%Y-%m-%d %H:%M:%S') .. ' - ' .. message .. '\n')
+        file:close()
+    end
+end
+
+return Logger

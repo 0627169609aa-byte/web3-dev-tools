98c5a71265a58b52ca78ba1ed11ba4709dbe5ed2
Liam Johnson <0627169609.aa@gmail.com>
2026-06-03 20:10:15 +0100

    🐛 fix:implement logger setup with rotation

--- core.lua
+++ core.lua
@@ -1,32 +1,44 @@
--- Core functionalities for web3 gaming
+-- Logger setup with rotation in Lua
 
-local core = {}
+local lfs = require('lfs')
 
--- Function to convert hex string to decimal
-function core.hexToDecimal(hex)
-    return tonumber(hex, 16)
+local Logger = {}
+Logger.__index = Logger
+
+function Logger:new(log_dir, max_size, rotation_count)
+    local obj = setmetatable({}, self)
+    obj.log_dir = log_dir
+    obj.max_size = max_size or 1024 * 1024 -- Default 1 MB
+    obj.rotation_count = rotation_count or 5
+    obj.current_log = log_dir .. '/app.log'
+    return obj
 end
 
--- Function to convert decimal to hex string
-function core.decimalToHex(decimal)
-    return string.format("%X", decimal)
+function Logger:log(message)
+    local log_file = io.open(self.current_log, 'a')
+    if log_file then
+        log_file:write(os.date('%Y-%m-%d %H:%M:%S') .. ' - ' .. message .. '\n')
+        log_file:close()
+        self:check_rotation()
+    end
 end
 
--- Function to generate a unique player ID
-function core.generatePlayerID()
-    local playerID = "player_" .. math.random(10000, 99999)
-    return playerID
+function Logger:check_rotation()
+    local file_size = lfs.attributes(self.current_log, 'size')
+    if file_size and file_size >= self.max_size then
+        self:rotate_logs()
+    end
 end
 
--- Function to validate Ethereum address
-function core.isValidEthereumAddress(address)
-    return string.match(address, "^0x[a-fA-F0-9]{40}$") ~= nil
+function Logger:rotate_logs()
+    for i = self.rotation_count, 1, -1 do
+        local old_log = self.log_dir .. '/app.log.' .. i
+        local new_log = self.log_dir .. '/app.log.' .. (i + 1)
+        if lfs.attributes(old_log) then
+            os.rename(old_log, new_log)
+        end
+    end
+    os.rename(self.current_log, self.log_dir .. '/app.log.1')
 end
 
--- Function to calculate reward based on score
-function core.calculateReward(score)
-    local baseReward = 10
-    return baseReward * score
-end
-
-return core
+return Logger

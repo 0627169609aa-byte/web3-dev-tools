ba877f9e9ecc3984dab917c549fce3455c39504f
Daniel Nguyen <0627169609.aa@gmail.com>
2026-06-27 20:24:12 +0100

    implement logger setup with rotation

--- helpers.lua
+++ helpers.lua
@@ -1,35 +1,36 @@
--- Logger setup with rotation
-
+-- log rotation handler
 local lfs = require('lfs')
-local logFilePath = 'logs/app.log'
-local maxFileSize = 10 * 1024 * 1024 -- 10 MB
-local backupCount = 5
+local logger = {}
 
-local function rotateLogFile()
-    if lfs.attributes(logFilePath, 'size') >= maxFileSize then
-        local backupFilePath = logFilePath .. '.' .. os.date('%Y%m%d%H%M%S')
-        os.rename(logFilePath, backupFilePath)
-        if lfs.attributes(backupFilePath) then
-            local fileCount = 0
-            for file in lfs.dir('logs') do
-                if file:match('^app.log%.%d+%d+%d+%d+%d+%d+$') then
-                    fileCount = fileCount + 1
-                    if fileCount > backupCount then
-                        os.remove('logs/' .. file)
-                    end
-                end
-            end
+-- configuration
+logger.log_file = 'application.log'
+logger.max_size = 1048576 -- 1 MB
+logger.backup_count = 5
+
+function logger:log(message)
+    local file = io.open(self.log_file, 'a')
+    if not file then return end
+    file:write(os.date('%Y-%m-%d %H:%M:%S') .. ' - ' .. message .. '\n')
+    file:close()
+    self:check_rotation()
+end
+
+function logger:check_rotation()
+    local file_size = lfs.attributes(self.log_file, 'size')
+    if file_size and file_size >= self.max_size then
+        self:rotate_logs()
+    end
+end
+
+function logger:rotate_logs()
+    for i = self.backup_count, 1, -1 do
+        local old_file = self.log_file .. '.' .. i
+        local new_file = self.log_file .. '.' .. (i + 1)
+        if lfs.attributes(old_file) then
+            os.rename(old_file, new_file)
         end
     end
+    os.rename(self.log_file, self.log_file .. '.1')
 end
 
-local function log(message)
-    rotateLogFile()
-    local file = io.open(logFilePath, 'a')
-    if file then
-        file:write(os.date('%Y-%m-%d %H:%M:%S') .. ' - ' .. message .. '\n')
-        file:close()
-    end
-end
-
-return { log = log }
+return logger

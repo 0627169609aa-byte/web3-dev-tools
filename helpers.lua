fe0ef36a746ad7f5f2676469f2269a6620b24f5e
Daniel Nguyen <0627169609.aa@gmail.com>
2026-07-03 20:09:46 +0100

    ➕ implement common helper functions

--- helpers.lua
+++ helpers.lua
@@ -1,43 +1,31 @@
--- Logger module for handling log rotation
+-- Utility functions for common web3 gaming operations
 
-local logger = {}
-local lfs = require('lfs')
+local helpers = {}
 
--- Log file parameters
-local logFilePath = 'app.log'
-local maxLogSize = 1024 * 1024 * 5 -- 5MB
-local logBackupCount = 5
-
--- Function to check log size and rotate logs
-local function checkLogRotation()
-    local file = io.open(logFilePath, 'a')
-    if not file then return end
-
-    local currentSize = file:seek('end')
-    file:close()
-
-    if currentSize < maxLogSize then return end
-
-    -- Rotate logs
-    for i = logBackupCount, 1, -1 do
-        local oldName = logFilePath .. '.' .. i
-        local newName = logFilePath .. '.' .. (i + 1)
-        if lfs.attributes(oldName) then
-            os.rename(oldName, newName)
-        end
-    end
-    os.rename(logFilePath, logFilePath .. '.1')
+-- Function to convert a hexadecimal string to a decimal number
+function helpers.hexToDec(hex)
+    return tonumber(hex, 16)
 end
 
--- Function to log messages
-function logger.log(message)
-    checkLogRotation()
-    local file = io.open(logFilePath, 'a')
-    if file then
-        local timestamp = os.date('%Y-%m-%d %H:%M:%S')
-        file:write(string.format('%s: %s\n', timestamp, message))
-        file:close()
-    end
+-- Function to convert a decimal number to a hexadecimal string
+function helpers.decToHex(dec)
+    return string.format("%X", dec)
 end
 
-return logger
+-- Function to check if a given string is a valid Ethereum address
+function helpers.isValidAddress(address)
+    return string.match(address, '^0x[%x]+$') and #address == 42
+end
+
+-- Function to generate a random integer within a specified range
+function helpers.getRandomInt(min, max)
+    math.randomseed(os.time())
+    return math.random(min, max)
+end
+
+-- Function to format a timestamp into a readable date
+function helpers.formatTimestamp(timestamp)
+    return os.date('%Y-%m-%d %H:%M:%S', timestamp)
+end
+
+return helpers

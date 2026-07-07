9ded24babf1e98ebdd213d283e0483fd4878af37
Daniel Nguyen <0627169609.aa@gmail.com>
2026-07-07 20:09:53 +0100

    ✨ feat:setup logger with rotation functionality

--- config.lua
+++ config.lua
@@ -1,42 +1,53 @@
--- Configuration for web3-dev-tools
+-- Logger setup with rotation using LuaFileSystem
+local lfs = require 'lfs'
 
-local Config = {}
+local Logger = {}
+Logger.__index = Logger
 
--- Default settings
-Config.settings = {
-    rpcUrl = "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
-    defaultGasLimit = 500000,
-    defaultGasPrice = 1000000000,
-}
-
--- Function to validate RPC URL
-local function isValidUrl(url)
-    return url and url:match('^https?://') ~= nil
+function Logger.new(log_dir, max_size, rotation_count)
+    local self = setmetatable({}, Logger)
+    self.log_dir = log_dir or './logs'
+    self.max_size = max_size or 1024 * 1024 -- 1 MB
+    self.rotation_count = rotation_count or 5
+    self:setup_log_directory()
+    self.current_log_file = self:generate_log_filename()
+    return self
 end
 
--- Function to set RPC URL
-function Config.setRpcUrl(newUrl)
-    if not isValidUrl(newUrl) then
-        error("Invalid RPC URL provided: " .. tostring(newUrl))
+function Logger:setup_log_directory()
+    if not lfs.attributes(self.log_dir) then
+        lfs.mkdir(self.log_dir)
     end
-    Config.settings.rpcUrl = newUrl
 end
 
--- Function to update gas settings
-function Config.updateGasSettings(gasLimit, gasPrice)
-    if type(gasLimit) ~= "number" or gasLimit <= 0 then
-        error("Gas limit must be a positive number")
+function Logger:generate_log_filename()
+    return string.format('%s/log_%s.txt', self.log_dir, os.date('%Y%m%d_%H%M%S'))
+end
+
+function Logger:log(message)
+    local file = io.open(self.current_log_file, 'a')
+    file:write(string.format('[%s] %s\n', os.date('%Y-%m-%d %H:%M:%S'), message))
+    file:close()
+    self:check_rotation()
+end
+
+function Logger:check_rotation()
+    local file_size = lfs.attributes(self.current_log_file, 'size')
+    if file_size and file_size >= self.max_size then
+        self:rotate_logs()
     end
-    if type(gasPrice) ~= "number" or gasPrice < 0 then
-        error("Gas price must be a non-negative number")
+end
+
+function Logger:rotate_logs()
+    for i = self.rotation_count, 1, -1 do
+        local old_file = string.format('%s/log_%d.txt', self.log_dir, i - 1)
+        local new_file = string.format('%s/log_%d.txt', self.log_dir, i)
+        if lfs.attributes(old_file) then
+            os.rename(old_file, new_file)
+        end
     end
-    Config.settings.defaultGasLimit = gasLimit
-    Config.settings.defaultGasPrice = gasPrice
+    os.rename(self.current_log_file, string.format('%s/log_0.txt', self.log_dir))
+    self.current_log_file = self:generate_log_filename()
 end
 
--- Function to retrieve current settings
-function Config.getSettings()
-    return Config.settings
-end
-
-return Config
+return Logger

d1056683580468b6757c56e023d1eb861c5b3c6b
Daniel Nguyen <0627169609.aa@gmail.com>
2026-07-23 20:40:35 +0100

    🐛 fix:implement logger setup with rotation

--- core.lua
+++ core.lua
@@ -1,40 +1,33 @@
--- Performance Optimizations in Core Module
+-- Logger setup with rotation
 
-local function calculateGameScore(playerActions)
-    local score = 0
-    local scoreMultiplier = 1.5  -- Example multiplier for performance
+local logging = require('logging')
+local logFile = 'app.log'
+local maxFileSize = 10 * 1024 * 1024  -- 10 MB
 
-    for _, action in ipairs(playerActions) do
-        if action.type == "kill" then
-            score = score + 100 * scoreMultiplier
-        elseif action.type == "collect" then
-            score = score + 50 * scoreMultiplier
-        elseif action.type == "assist" then
-            score = score + 75 * scoreMultiplier
-        end
+local function rotateLogs()
+    local stats = lfs.attributes(logFile)
+    if stats and stats.size >= maxFileSize then
+        local date = os.date('%Y-%m-%d_%H-%M-%S')
+        local newLogFile = string.format('app_%s.log', date)
+        os.rename(logFile, newLogFile)
     end
-
-    return score
 end
 
-local function updatePlayerStats(player, actions)
-    local newScore = calculateGameScore(actions)
-    player.score = player.score + newScore
-    player.lastUpdated = os.time()
+local function logMessage(level, message)
+    rotateLogs()  -- Check and rotate logs if necessary
+    local logger = logging.file(logFile)
+    logger:log(level, message)
 end
 
-local function optimizeDataStorage(data)
-    local compactedData = {}
-    for key, value in pairs(data) do
-        if value ~= nil then
-            compactedData[key] = value
-        end
-    end
-    return compactedData
+local function info(message)
+    logMessage(logging.INFO, message)
+end
+
+local function error(message)
+    logMessage(logging.ERROR, message)
 end
 
 return {
-    calculateGameScore = calculateGameScore,
-    updatePlayerStats = updatePlayerStats,
-    optimizeDataStorage = optimizeDataStorage
+    info = info,
+    error = error,
 }

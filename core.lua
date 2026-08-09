ea3f028f4f1bc00e327ef1ba44835d948f0b4798
Daniel Nguyen <0627169609.aa@gmail.com>
2026-08-09 20:09:12 +0100

    ➕ logger setup with rotation

--- core.lua
+++ core.lua
@@ -1,50 +1,43 @@
--- Utility functions for gaming data handling
+-- Logger setup with rotation
 
--- Function to calculate the player's score based on various factors
--- @param baseScore Number representing the base score of the player
--- @param levelBonus Number representing the bonus score for the level
--- @param multiplier Number representing any multipliers applied
--- @return Number representing the total score
-function calculateScore(baseScore, levelBonus, multiplier)
-    local totalScore = (baseScore + levelBonus) * multiplier
-    return totalScore
+local log = require('log')
+local lfs = require('lfs')
+
+local Logger = {}
+Logger.__index = Logger
+
+function Logger:new(logFilePath, maxSize, backupCount)
+    local instance = setmetatable({}, Logger)
+    instance.logFilePath = logFilePath
+    instance.maxSize = maxSize or 1024 * 1024 -- 1 MB default
+    instance.backupCount = backupCount or 5
+    return instance
 end
 
--- Function to format player stats into a readable string
--- @param playerName String representing the name of the player
--- @param playerLevel Number representing the level of the player
--- @param playerScore Number representing the score of the player
--- @return String formatted player stats
-function formatPlayerStats(playerName, playerLevel, playerScore)
-    return string.format("Player: %s | Level: %d | Score: %d", playerName, playerLevel, playerScore)
-end
+function Logger:rotateLogs()
+    local file = io.open(self.logFilePath, 'r')
+    if file then
+        local size = file:seek('end')
+        file:close()
 
--- Function to validate gaming data before processing
--- @param data Table containing gaming data to validate
--- @return Boolean indicating if the data is valid
-function validateGamingData(data)
-    if not data.playerName or type(data.playerName) ~= "string" then
-        return false
+        if size >= self.maxSize then
+            for i = self.backupCount, 1, -1 do
+                local oldFile = string.format('%s.%d', self.logFilePath, i)
+                local newFile = string.format('%s.%d', self.logFilePath, i + 1)
+                if lfs.attributes(oldFile) then
+                    os.rename(oldFile, newFile)
+                end
+            end
+            os.rename(self.logFilePath, string.format('%s.1', self.logFilePath))
+        end
     end
-    if not data.playerLevel or type(data.playerLevel) ~= "number" then
-        return false
-    end
-    if not data.playerScore or type(data.playerScore) ~= "number" then
-        return false
-    end
-    return true
 end
 
--- Example use case of the utility functions
-local examplePlayerData = {
-    playerName = "Hero",
-    playerLevel = 5,
-    playerScore = 200
-}
-
-if validateGamingData(examplePlayerData) then
-    local score = calculateScore(examplePlayerData.playerScore, 50, 2)
-    print(formatPlayerStats(examplePlayerData.playerName, examplePlayerData.playerLevel, score))
-else
-    print("Invalid player data.")
+function Logger:log(message)
+    self:rotateLogs()
+    local file = io.open(self.logFilePath, 'a')
+    file:write(os.date('%Y-%m-%d %H:%M:%S') .. ' - ' .. message .. '\n')
+    file:close()
 end
+
+return Logger

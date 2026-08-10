af13588c6dd70d4b59db77d5564d7a8a5d8bf9c8
Daniel Nguyen <0627169609.aa@gmail.com>
2026-08-10 20:09:11 +0100

    ✨ feat:input validation for transaction data

--- helpers.lua
+++ helpers.lua
@@ -1,63 +1,37 @@
--- logger.lua
-local Logger = {}
+-- Input validation for web3 transaction data
 
--- log levels
-Logger.levels = {
-    DEBUG = 1,
-    INFO = 2,
-    WARN = 3,
-    ERROR = 4,
-    FATAL = 5,
+local function isValidAddress(address)
+    if type(address) ~= 'string' then return false end
+    -- Simple regex for Ethereum addresses
+    return address:match('^0x[a-fA-F0-9]{40}$') ~= nil
+end
+
+local function isValidAmount(amount)
+    return type(amount) == 'number' and amount > 0
+end
+
+local function validateTransactionInput(transaction)
+    if not isValidAddress(transaction.from) then
+        return false, 'Invalid sender address'
+    end
+    if not isValidAddress(transaction.to) then
+        return false, 'Invalid receiver address'
+    end
+    if not isValidAmount(transaction.value) then
+        return false, 'Invalid transaction amount'
+    end
+    return true
+end
+
+local function processTransaction(transaction)
+    local isValid, errMsg = validateTransactionInput(transaction)
+    if not isValid then
+        error(errMsg)
+    end
+    -- Proceed with transaction processing logic here...
+end
+
+return {
+    validateTransactionInput = validateTransactionInput,
+    processTransaction = processTransaction
 }
-
--- default log level
-Logger.currentLevel = Logger.levels.INFO
-
--- set log level
-function Logger:setLevel(level)
-    self.currentLevel = level
-end
-
--- check log level
-function Logger:shouldLog(level)
-    return level >= self.currentLevel
-end
-
--- log message with rotation
-function Logger:log(level, message)
-    if self:shouldLog(level) then
-        local timestamp = os.date('%Y-%m-%d %H:%M:%S')
-        local logMessage = string.format('%s [%s] %s', timestamp, level, message)
-
-        -- write log to file with rotation
-        local logfile = 'app.log'
-        local file = io.open(logfile, 'a')
-        if file then
-            file:write(logMessage .. '\n')
-            file:close()
-        end
-         
-        -- check log file size and rotate if necessary
-        if self:checkFileSize(logfile) then
-            self:rotateLogFile(logfile)
-        end
-    end
-end
-
--- check if log file exceeds size limit
-function Logger:checkFileSize(filename)
-    local file = io.open(filename, 'r')
-    if file then
-        local size = file:seek('end')
-        file:close()
-        return size > 1024 * 1024 -- 1 MB limit
-    end
-    return false
-end
-
--- rotate log file
-function Logger:rotateLogFile(filename)
-    os.rename(filename, filename .. '.' .. os.date('%Y%m%d%H%M%S'))
-end
-
-return Logger
